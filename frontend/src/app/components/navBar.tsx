50d24d6a8e4626edbcba17b3cd481596ccaba0d6
Talia-goncalves <gtalia246@gmail.com>
2024-08-02 19:08:57 -0300

    feat: criando navbar do site
    criado navegacao onde ficará os links de todas as paginas do site

---
+++ frontend/src/app/components/navBar.tsx
@@ -0,0 +1,26 @@
+import { FaTools, FaUsers } from "react-icons/fa";
+import { FaPeopleRoof } from "react-icons/fa6";
+import { GrHostMaintenance } from "react-icons/gr";
+import { MdDashboard, MdForklift } from "react-icons/md";
+
+export function NavBar(){
+    const links = [
+        {target: "", text: "Estoque", icon: <MdDashboard/>},
+        {target: "#", text: "Máquinas", icon: <MdForklift/>},
+        {target: "#", text: "Manutenções", icon: <GrHostMaintenance/>},
+        {target: "#", text: "Peças e Materiais", icon: <FaTools/>},
+        {target: "#", text: "Equipes", icon: <FaPeopleRoof/>},
+        {target: "#", text: "Usuário", icon: <FaUsers/>}
+    ];
+
+    return(
+        <nav className="space-y-4 flex flex-col mt-8">
+            {links.map((obj) => (
+                <a href={obj.target} className="flex items-center gap-4 hover:font-semibold">
+                    {obj.icon}
+                    {obj.text}
+                </a>
+            ))}
+        </nav>
+    );
+}
