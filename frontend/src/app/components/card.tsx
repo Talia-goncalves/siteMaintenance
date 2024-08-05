b5855181b74cc09f37a302e3f5b1fb9cd8b25f1b
Talia-goncalves <gtalia246@gmail.com>
2024-08-05 13:05:14 -0300

    fix: corrigindo o tamanho dos cards para que fiquem todos iguais

--- frontend/src/app/components/card.tsx
+++ frontend/src/app/components/card.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 interface CardInterface {
-    color?: string;    // ? significa que o parâmetro é opcional se fosse ! seria obrigatório
-    qty?: string;
-    text?: string;
-    icon?: React.ReactNode; // ou any
+  color?: string;
+  qty?: string;
+  text?: string;
+  icon?: React.ReactNode;
 }
 
-export function Card({color, qty, text, icon}: CardInterface){
-    return(
-        <div className={`${color} p-6 flex gap-2 rounded-xl`}>
-            <div className="flex-1 flex flex-col">
-                  <strong className="text-3xl font-bold">{qty}</strong>
-                  <span className="font-medium text-white ">{text}</span>
-            </div>
-            {icon}
-        </div>
-    )
+export function Card({ color = "bg-gray-800", qty, text, icon }: CardInterface) {
+  return (
+    <div className={`flex items-center justify-between ${color} p-6 rounded-xl h-24 w-56`}>
+      <div className="flex-1 flex flex-col justify-center">
+        {qty && <strong className="text-3xl font-bold text-white">{qty}</strong>}
+        {text && <span className="font-medium text-white">{text}</span>}
+      </div>
+      {icon && <div className="text-white text-3xl">{icon}</div>}
+    </div>
+  );
 }
